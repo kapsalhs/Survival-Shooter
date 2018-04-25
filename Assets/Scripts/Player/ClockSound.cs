c2b370f31f29b90716b4782fc41c89a1b8c51ef2
kapsalhs <kapsalhs1997@gmail.com>
2018-04-25 03:17:36 +0300

    Light ans clock

---
+++ Assets/Scripts/Player/ClockSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockSound : MonoBehaviour {
+
+
+	AudioSource clockSound;
+
+	void Awake () {
+		clockSound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.CompareTag ("Player")) {
+			clockSound.Play ();
+		}
+	}
+}
